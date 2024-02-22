eacba8b69893ba0e7743e43eb8475a11ac178dbf
pink-cypher <143744116+pink-cypher@users.noreply.github.com>
2024-02-22 12:35:10 -0700

    Uimain draft update

--- src/uimain.c
+++ src/uimain.c
@@ -7,42 +7,25 @@ int main()
 
   while (1) { // Infinite while loop
 
-    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c, Arrow = a), or 'q' to quit\n> ", stdout);
+    fputs("Please input a sentence, 'h' to see your history or 'q' to quit\n> ", stdout);
     fflush(stdout);		/* stdout only flushes automatically on \n */
     int c;
     while ((c = getchar()) == '\n'); /* ignore newlines */
     if (c == EOF)		     /* terminate on end-of-file */
       goto done;
 
-    // Given the user answer, select which method to call
-    switch (c) {
-    case 't':
-      puts("You selected triangle:");
-      print_triangle(5, 7);
-      break;
-    case 's':
-      puts("You selected square:");
-      print_square(5, 5);
-      break;
-    case 'c':
-      puts("You selected chars:");
-      for (char c = 'A'; c < 'D'; c++)
-	print_char_8x12(c);
-      break;
-    case 'a':
-      puts("You selected arrow:");
-      print_triangle(5,7);
-      print_square(10,5);
-      break;
-    case 'q':
+    // Given the user answer, as long as it is one of the options, it will call the function
+    if (c == 'h'){
+      //return history
+    }
+    else if (c == 'q'){
       puts("Bye!");
       goto done; 		/* terminate */
-    case '\n':
-      break;
-    default:
-      printf("Unrecognized option '%c', please try again!\n", c);
     }
-  }
+    else{
+      //tokenize the sentence input
+
+    }
 
  done: // To exit from program
   return 0;
