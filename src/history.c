6e5ec25b72f726b34fee2f0d782a4be9f73cc803
pink-cypher <143744116+pink-cypher@users.noreply.github.com>
2024-02-22 12:17:33 -0700

    Updated commit

--- src/history.c
+++ src/history.c
@@ -20,13 +20,16 @@ List* init_history(){
 */
 void add_history(List *list, char *str);
 
+
 /* Retrieve the string stored in the node where Item->id == id.                                                                                                                                         
 List* list - the linked list
 int id - the id of the Item to find */
 char *get_history(List *list, int id);
 
+
 /*Print the entire contents of the list. */
 void print_history(List *list);
+
 
 /*Free the history list and the strings it references. */
 void free_history(List *list);~
---
+++ src/uimain.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include "tokenizer.h"
+
+int main() 
+{
+  puts("Welcome to Isabella's Tokenizer!");
+
+  while (1) { // Infinite while loop
+
+    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c, Arrow = a), or 'q' to quit\n> ", stdout);
+    fflush(stdout);		/* stdout only flushes automatically on \n */
+    int c;
+    while ((c = getchar()) == '\n'); /* ignore newlines */
+    if (c == EOF)		     /* terminate on end-of-file */
+      goto done;
+
+    // Given the user answer, select which method to call
+    switch (c) {
+    case 't':
+      puts("You selected triangle:");
+      print_triangle(5, 7);
+      break;
+    case 's':
+      puts("You selected square:");
+      print_square(5, 5);
+      break;
+    case 'c':
+      puts("You selected chars:");
+      for (char c = 'A'; c < 'D'; c++)
+	print_char_8x12(c);
+      break;
+    case 'a':
+      puts("You selected arrow:");
+      print_triangle(5,7);
+      print_square(10,5);
+      break;
+    case 'q':
+      puts("Bye!");
+      goto done; 		/* terminate */
+    case '\n':
+      break;
+    default:
+      printf("Unrecognized option '%c', please try again!\n", c);
+    }
+  }
+
+ done: // To exit from program
+  return 0;
+}
