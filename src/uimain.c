6344901e6d24552977a11f63ef82999126e287b5
pink-cypher <143744116+pink-cypher@users.noreply.github.com>
2024-03-19 16:35:34 -0600

    uimain updates

--- src/uimain.c
+++ src/uimain.c
@@ -5,34 +5,35 @@ int main() {
     puts("Welcome to Isabella's Tokenizer!");
 
     while (1) { // Infinite while loop
-        fputs("Please input a sentence, 'h' to see your history or 'q' to quit\n> ", stdout);
-        fflush(stdout);             /* stdout only flushes automatically on \n */
-        int c = getchar();
-	
-        while (getchar() != '\n'); /* ignore newlines */
+        fputs("Please input 's' to enter a sentence, 'h' to see your history or 'q' to quit\n> ", stdout);
+        fflush(stdout);/* stdout only flushes automatically on \n */
+	int c = getchar();
+		
+        while (c != '\n'|| c == EOF){/* ignore newlines */
+	  c = getchar();
 
-        if (c == EOF)                    /* terminate on end-of-file */
-            break;
+	}
 
-        // Given the user answer, as long as it is one of the options, it will call the function
-        else if (c == 'h'){
-	  puts("Functionality for viewing history will be implemented here.");
-            //return history
-        }
-        else if (c == 'q'){
-            puts("Bye!");
-            break;                /* terminate */
-        }
-        else{
-            char sentence[100]; // Assuming maximum sentence length is 100 characters
-	    fputs("Please input a sentence: ", stdout);
+	switch(c){
+	  case 's':
+	    fputs("Please enter your string: ", stdout);
 	    fflush(stdout);
-            fgets(sentence, sizeof(sentence), stdin); // Read user input
-            char **tokens = tokenize(sentence); // Tokenize the sentence
-            print_tokens(tokens); // Print the tokens
-            free_tokens(tokens); // Free memory allocated for tokens
+	    char sentence[100];// Assuming maximum sentence length is 100 characters                                                                                                                       
+            getchar();
+            fgets(sentence, sizeof(sentence), stdin); // Read user input                                                                                                                                   
+            printf("You inputed: %s", sentence);
+            char **tokens = tokenize(sentence); // Tokenize the sentence                                                                                                                                   
+            print_tokens(tokens); // Print the tokens   
 
-        }
+	  case 'h':
+	    // Given the user answer, as long as it is one of the options, it will call the function
+	   puts("Functionality for viewing history will be implemented here.");
+	   continue; //return history
+
+	case 'q':
+	   puts("Bye!");
+           break;                /* terminate */
+	}
     }
 
     // To exit from program
